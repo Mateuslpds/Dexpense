0d0b5cac5f605df3174dccc5c978f58239f79c10
Dudu200313 <eduardo.silvinocabuloso23@gmail.com>
2024-09-11 21:00:45 -0300

    front da tela editExpense

--- app/(logged)/editExpense.tsx
+++ app/(logged)/editExpense.tsx
@@ -1,18 +1,22 @@
 import { db } from '../../firebaseConfig';
 import { useRouter, useLocalSearchParams } from 'expo-router';
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
 import { doc, updateDoc } from 'firebase/firestore';
+import { View, TextInput, TouchableOpacity, Text, Image, StyleSheet  } from 'react-native';
+
 
 export default function EditExpenseScreen() {
   const { id, description: initialDescription, value: initialValue } = useLocalSearchParams();
+
 
   const [description, setDescription] = useState(initialDescription || '');
   const [value, setValue] = useState(initialValue ? initialValue.toString() : '');
   const router = useRouter();
 
+
   const handleSave = async () => {
     if (!id) return;
+
 
     const expenseRef = doc(db, 'expenses', id);
     await updateDoc(expenseRef, {
@@ -22,28 +26,91 @@ export default function EditExpenseScreen() {
     router.back();
   };
 
+
   return (
-    <View style={{ flex: 1, padding: 20 }}>
+   
+    <View style={styles.container}>
+
+
+      <Image
+        source={require('../../assets/images/logo_dexpense.png')}
+        style={styles.image}
+      />
       <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Editar Despesa</Text>
 
-      <Text style={{ marginVertical: 10 }}>Descrição Atual: {initialDescription}</Text>
 
-      <Text style={{ marginVertical: 10 }}>Quantia Atual: {initialValue}</Text>
+      <Text style={{ marginVertical: 10, fontWeight: 'bold'}}>Descrição Atual: {initialDescription}</Text>
+
+
+      <Text style={{ marginVertical: 10,fontWeight: 'bold' }}>Quantia Atual: {initialValue}</Text>
+
 
       <TextInput
-        style={{ borderWidth: 1, marginVertical: 10, padding: 5 }}
+        style={styles.input}
         value={description}
+       
         onChangeText={setDescription}
         placeholder="Descrição"
       />
       <TextInput
-        style={{ borderWidth: 1, marginVertical: 10, padding: 5 }}
+        style={styles.input}
         value={value}
         onChangeText={setValue}
         keyboardType="numeric"
         placeholder="Valor"
       />
-      <Button title="Salvar" onPress={handleSave} />
+
+
+      <TouchableOpacity
+        style={styles.btngreen}
+        onPress={handleSave}>
+        <Text style={styles.buttonText}>Salvar</Text>
+      </TouchableOpacity>
+
+
     </View>
   );
 }
+ 
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btngreen: { //Style do botão
+    backgroundColor: '#9FD078',  
+    paddingVertical: 12,  
+    paddingHorizontal: 28,
+    marginTop: 10,
+    borderRadius:50,
+    borderWidth: 1,
+    borderColor: '#333',
+    width: 250,
+  },
+  buttonText: { //Style do texto do botão
+    color: 'black',
+    fontSize: 18,  
+    fontWeight: 'bold',  
+    textAlign: 'center',  
+  },
+
+
+  image: { //imagem do logo
+    width: 200,
+    height: 200,
+
+
+  },
+  input: {
+    width: 250,
+    height: 50,
+    borderColor: 'black',
+    borderRadius:50,
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 20
+  },
+})
